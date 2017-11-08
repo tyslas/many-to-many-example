6f14bb2e00d47332846d8c80efd3ac80b878e8f7
coloradokim <schlesinger.kim@gmail.com>
2017-11-08 10:12:30 -0700

    remove solution

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/movies'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/movies', function(req, res, next) {
+  return dbConnection.select().table('movie')
+  .then((movieData) => {
+    res.status(200).json({
+      status: 'success',
+      data: movieData
+    });
+  });
+});
+
 router.get('/movies/:id', function(req, res, next) {
-  dbConnection('movie').where('movie.id', req.params.id)
-  .select('movie.title', 'actor.name')
-  .innerJoin('actor_movie', 'movie.id', 'actor_movie.movie_id')
-  .innerJoin('actor', 'actor.id', 'actor_movie.actor_id')
+// write code here
   .then((movieInfo) => {
     res.status(201).json({
       status: 'success',
